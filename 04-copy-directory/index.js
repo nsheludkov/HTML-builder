6aac8c8689b1f952a22ff7a19fbf1f7497ab377b
nsheludkov <nsheludkov@ya.ru>
2023-05-08 18:47:19 +0300

    feat: 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const fsPromises= require('fs/promises');
+const fs = require('fs');
+const path = require('path');
+
+async function copy(file) {
+try {
+  await fsPromises.copyFile(path.join(__dirname,'files',file.name), path.join(copyDir, file.name));
+  console.log(`${file.name} was copied to files-copy`);
+} catch {
+  console.error('The file could not be copied');
+}
+}
+
+copyDir = path.join(__dirname, 'files-copy');
+filesDir = path.join(__dirname, 'files');
+fs.mkdir(copyDir, { recursive: true }, err => {
+  if (err) throw err; // не удалось создать папку
+  fs.readdir( filesDir,
+    { withFileTypes: true },
+    (err, files) => {
+      if (err)
+        console.log(err);
+      else {
+        files.forEach(file => {
+          if (file.isFile()) {
+            copy(file);
+          }
+        })
+      }
+    });
+});
 
